9fc11f2ecf978b86d60d1d009a804f8495713552
Répai Attila <repai.attila@hallgato.ppke.hu>
2025-08-25 15:13:32 +0200

    read_test menet közbeni kiírások nélkül

--- tests/read_test.cpp
+++ tests/read_test.cpp
@@ -24,12 +24,16 @@ int main(int argc, char *argv[]) {
     }
 
     const auto ser = exoskeleton::motor::open_serial_port(exoskeleton::motor::find_port_name_by_serial_num("CSTNY004"));
-    for (int i = 0 ; i < 1000; i++) {
+    const size_t n {1000};
+    std::vector<decltype(std::chrono::milliseconds{1}.count())> times(n);
+    for (auto i = 0 ; i < n; i++) {
         auto t0 = std::chrono::steady_clock::now();
         auto data = exoskeleton::motor::read_data(*ser,3);
         auto t1 = std::chrono::steady_clock::now();
         auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0);
-        std::cerr << dur.count() << "ms " << data << std::endl;
+        times[i] = dur.count();
+        // std::cerr << dur.count() << "ms " << data << std::endl;
     }
+    std::cout << static_cast<long double>(std::reduce(times.begin(), times.end())) / n << std::endl;
     return app.exec();
 }
